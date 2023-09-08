af6d4dd709c2c93e3c0554199f53b8ca8cc7f422
João Túlio <joaotuliosoares@hotmail.com>
2023-09-08 10:38:26 +0000

    create `in construction` page

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,24 @@
+import styles from "../styles/home.module.css";
+
 function Home() {
   return (
-    <h1>Futura implementação do Tabnews, o curso do Deschamps é muito bom!</h1>
+    <main className={styles.container}>
+      <div className={styles.content}>
+        <h1>AgroTab - Conteúdos do mundo Agro</h1>
+        <p className={styles.phrase}>
+          O seu portal <b>confiável</b> para informações <b>atualizadas</b>{" "}
+          sobre o mundo da <b>agricultura</b>
+        </p>
+        <p>
+          Seja você um agricultor experiente, um entusiasta da vida rural ou
+          alguém interessado em aprender mais sobre o setor agrícola, você está
+          no lugar certo! Estamos empolgados em apresentar o nosso blog de
+          notícias dedicado ao mundo da agricultura, onde você encontrará
+          informações, insights e análises sobre os mais recentes
+          desenvolvimentos e tendências no campo agrícola.
+        </p>
+      </div>
+    </main>
   );
 }
 
